11c02208511ee36cd1dadbdcb60613297a6c4b81
josh098741 <morongemokaya@gmail.com>
2025-09-22 01:39:30 +0300

    Additional commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -16,5 +18,23 @@ const UserSchema = new mongoose.Schema({
         required: [true, "Please enter your password"]
     }
 })
+
+UserSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+UserSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+UserSchema.methods.generateAuthToken = function(){
+    return jwt.sign(
+        {id: this._id, username: this.username},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_DATE}
+    );
+};
 
 module.exports = mongoose.model('User',UserSchema)
