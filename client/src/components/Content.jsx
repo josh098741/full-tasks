a5d7273801bceea4d5ab6ae4d3e5afca646f04f0
josh098741 <morongemokaya@gmail.com>
2025-10-06 17:42:57 +0300

    Additional commits

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Content(){
+
+    const [task, setTask] = useState([])
+
     return(
         <div className="flex justify-center h-80 border-4 mx-10 my-20 items-center">
             <div className="w-1/2 h-72 border-2 rounded-lg m-2">
                 <h1 className="text-center text-xl font-bold">Task Input</h1>
+                <div className='flex justify-center'>
+                    <input className="border h-11 w-80 rounded-lg border-black" type="text"/>
+                </div>
+                <div>
+                    <button></button>
+                </div>
             </div>
             <div className="w-1/2 h-72 border-2 rounded-lg m-2">
                 <h1 className="text-center text-xl font-bold">Task View</h1>
+                <div></div>
             </div>
         </div>
     );
