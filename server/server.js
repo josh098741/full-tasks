f569996a6bd03f3fddc06cf460113f7cc102645c
josh098741 <morongemokaya@gmail.com>
2025-10-06 19:01:43 +0300

    Additional commits

--- server/server.js
+++ server/server.js
@@ -1,3 +1,20 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+
+const PORT = process.env.PORT || 5000
+const start = async () => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server stated on port ${PORT}`)
+        })
+    }catch(error){
+
+    }
+}
+start()
