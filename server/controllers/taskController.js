c2ba984f1fb8d00e2c477c0eb3852978454b29d9
josh098741 <morongemokaya@gmail.com>
2025-10-06 19:47:10 +0300

    Additional commits

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,14 +1,44 @@
 const Task = require('../models/Task')
 
-const createTask = async (req,res) => {}
+const createTask = async (req,res) => {
+    try{
 
-const getTasks = async (req,res) => {}
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in create task"})
+    }
+}
 
-const getTask = async (req,res) => {}
+const getTasks = async (req,res) => {
+    try{
 
-const updateTask = async (req,res) => {}
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in get Tasks"})
+    }
+}
 
-const deleteTask = async (req,res) => {}
+const getTask = async (req,res) => {
+    try{
+
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in get task"})
+    }
+}
+
+const updateTask = async (req,res) => {
+    try{
+
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in update task"})
+    }
+}
+
+const deleteTask = async (req,res) => {
+    try{
+
+    }catch(error){
+        res.status(500).json({success: false, message: "There was an error in delete task"})
+    }
+}
 
 module.exports = {
     createTask,
