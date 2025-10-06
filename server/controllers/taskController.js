3437b091ded93ce819932168bd9c752405c67fcf
josh098741 <morongemokaya@gmail.com>
2025-10-06 19:35:02 +0300

    Additional commitsm to task controller

---
+++ server/controllers/taskController.js
@@ -0,0 +1,19 @@
+const Task = require('../models/Task')
+
+const createTask = async (req,res) => {}
+
+const getTasks = async (req,res) => {}
+
+const getTask = async (req,res) => {}
+
+const updateTask = async (req,res) => {}
+
+const deleteTask = async (req,res) => {}
+
+module.exports = {
+    createTask,
+    getTasks,
+    getTask,
+    updateTask,
+    deleteTask
+}
